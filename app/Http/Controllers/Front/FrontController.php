8b377241d0229e516c5fb1bf5a65826e18a6effd
afryn <76405702+afryn@users.noreply.github.com>
2025-12-24 15:52:26 +0530

    deployment check vendor

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -112,7 +112,7 @@ class FrontController extends Controller
     }
     public function testing(Request $request)
     {
-        echo 'hello from gh actions';
+        echo 'hello from gh actions vr';
         $controller = new ProfileController();
         return $controller->buyModule(26, '6');
         die;
