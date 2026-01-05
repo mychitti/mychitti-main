526bd049bb3294914a07eeef03dd90c8e3b6e271
afryn <76405702+afryn@users.noreply.github.com>
2026-01-05 14:46:49 +0530

    added punch in reminder

--- app/Jobs/PunchInReminder.php
+++ app/Jobs/PunchInReminder.php
@@ -9,7 +9,7 @@ use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\InteractsWithQueue; 
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
