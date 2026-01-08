526cf8011608e66db59e38b9a4e17e391ec81df8
afryn <76405702+afryn@users.noreply.github.com>
2026-01-08 11:31:38 +0530

    adjustments for vendor google oauth

--- config/services.php
+++ config/services.php
@@ -32,6 +32,8 @@ return [
     'google' => [
         'client_id'     => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect'      => env('GOOGLE_DEFAULT_REDIRECT'),
+
     ],
 
     'google_redirects' => [
