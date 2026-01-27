7695c247f70eb63aa7f957c4d79e885fdc035a28
afryn <76405702+afryn@users.noreply.github.com>
2026-01-27 19:23:32 +0530

    updated

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title> @yield('title')</title>
+        <title>1 @yield('title')</title>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
