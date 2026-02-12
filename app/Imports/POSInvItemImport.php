a47f726a87fac33bce08de52cd46d687c3397740
afryn <76405702+afryn@users.noreply.github.com>
2026-02-12 14:50:09 +0530

    updated

--- app/Imports/POSInvItemImport.php
+++ app/Imports/POSInvItemImport.php
@@ -11,8 +11,8 @@ use App\Models\TempItemImage;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 
-class POSItemImport implements ToCollection
-{ 
+class POSItemImport implements ToCollection 
+{
     public $failedRows = [];
     protected $storeId;
 
@@ -34,7 +34,7 @@ class POSItemImport implements ToCollection
 
                 // new branch if not exists 
                 // if (is_numeric($branch)) {
-                    $branchId = $row[4];
+                $branchId = $row[4];
                 // } else {
                 //     $existingBranch = Branch::where('name', $branch)
                 //         ->where('store_id', $this->storeId)
@@ -52,38 +52,37 @@ class POSItemImport implements ToCollection
                 // }
                 // prx( $submittedItems);
 
+                $price = $row[1] ?? 0;
+                $qty = $row[2] ?? 0;
+                $gst_percent = $row[3] ?? 0;
+                // prx($price, $qty);
+                $invItem = BranchInventoryItem::where([
+                    'branch_id'          => $branchId,
+                    'inventory_item_id'  => $row[0]
+                ])->first();
 
-                    $price = $row[1] ?? 0;
-                    $qty = $row[2] ?? 0;
-                    $gst_percent = $row[3] ?? 0;
-                    // prx($price, $qty);
-                    $invItem = BranchInventoryItem::where([
+                if ($invItem) {
+                    BranchInventoryItem::where([
                         'branch_id'          => $branchId,
-                        'inventory_item_id'  => $row[0]
-                    ])->first();
-
-                    if ($invItem) {
-                        BranchInventoryItem::where([
-                            'branch_id'          => $branchId,
-                            'inventory_item_id'  => $row[0],
-                            'store_id'  => $this->storeId
-                        ])->update([
-                            'gst_percent' => $gst_percent,
-                            'qty'       => $invItem->qty + $qty,
-                            'qty_left'  => $invItem->qty_left + $qty,
-                            'price'     => $price,
-                        ]);
-                    } else {
-                        BranchInventoryItem::create([
-                            'gst_percent'                => $gst_percent,
-                            'branch_id'          => $branchId,
-                            'inventory_item_id'  => $row[0],
-                            'qty'                => $qty,
-                            'qty_left'           => $qty,
-                            'price'              => $price,
-                            'store_id'           => $this->storeId
-                        ]);
-                    }
+                        'inventory_item_id'  => $row[0],
+                        'store_id'  => $this->storeId
+                    ])->update([
+                        'gst_percent' => $gst_percent,
+                        'qty'       => $invItem->qty + $qty,
+                        'qty_left'  => $invItem->qty_left + $qty,
+                        'price'     => $price,
+                    ]);
+                } else {
+                    BranchInventoryItem::create([
+                        'gst_percent'                => $gst_percent,
+                        'branch_id'          => $branchId,
+                        'inventory_item_id'  => $row[0],
+                        'qty'                => $qty,
+                        'qty_left'           => $qty,
+                        'price'              => $price,
+                        'store_id'           => $this->storeId
+                    ]);
+                }
             } catch (\Exception $e) {
                 $this->failedRows[] = [
                     'row'    => $index + 1,
