49d86b4cc820875f67bf0e2a10b788f26f670d28
afryn <76405702+afryn@users.noreply.github.com>
2026-02-26 16:32:07 +0530

    updated

--- app/Http/Controllers/Vendor/ServiceController.php
+++ app/Http/Controllers/Vendor/ServiceController.php
@@ -1645,7 +1645,10 @@ class ServiceController extends Controller
                     'otp' => $otp,
                     'created_at' => now()
                 ]);
-                _send_confirmation_sms('mobile_verification', $userPhone->phone, $otp);
+                echo $userPhone->phone;
+               prx( _send_confirmation_sms('mobile_verification', $userPhone->phone, $otp));
+            }else{
+                prx('User not found for sending SMS');
             }
 
             $empJob = VendorEmpJob::where('service_id', $service_id)->first();
