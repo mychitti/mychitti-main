e285ab9de2b18eac250cb552ca5307308fa252b0
afryn <76405702+afryn@users.noreply.github.com>
2026-03-10 12:41:42 +0530

    updated

--- app/Http/Controllers/Front/SitemapController.php
+++ app/Http/Controllers/Front/SitemapController.php
@@ -16,44 +16,50 @@ class SitemapController extends Controller
         $categories = DB::table('categories')->where('status', 1)->get();
         $stores = DB::table('stores')->where('status', 1)->get();
 
+        // Use staging domain if on staging, otherwise production
+        $host = request()->getHost();
+        $baseUrl = $host === 'staging.mychitti.net'
+            ? 'https://staging.mychitti.net'
+            : 'https://mychitti.net'; 
+
         // Start XML structure
         $xml = '<?xml version="1.0" encoding="UTF-8"?>';
         $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
         // Add static URLs
-        $xml .= '<url><loc>' . url('/') . '</loc></url>';
-        $xml .= '<url><loc>' . url('/about-us') . '</loc></url>';
-        $xml .= '<url><loc>' . url('/contact') . '</loc></url>';
-        $xml .= '<url><loc>' . url('/list-your-business') . '</loc></url>';
-        $xml .= '<url><loc>' . url('/blog') . '</loc></url>';
-        $xml .= '<url><loc>' . url('/cart') . '</loc></url>';
+        $xml .= '<url><loc>' . $baseUrl . '/' . '</loc></url>';
+        $xml .= '<url><loc>' . $baseUrl . '/about-us</loc></url>';
+        $xml .= '<url><loc>' . $baseUrl . '/contact</loc></url>';
+        $xml .= '<url><loc>' . $baseUrl . '/list-your-business</loc></url>';
+        $xml .= '<url><loc>' . $baseUrl . '/blog</loc></url>';
+        $xml .= '<url><loc>' . $baseUrl . '/cart</loc></url>';
 
         // Add dynamic URLs from the database
         foreach ($items as $item) {
-            $xml .= '<url><loc>' . url('/tirupati3/' . $item->slug) . '</loc></url>';
+            $xml .= '<url><loc>' . $baseUrl . '/tirupati3/' . $item->slug . '</loc></url>';
         }
         foreach ($items as $item) {
-            $xml .= '<url><loc>' . url('/hyderabad/' . $item->slug) . '</loc></url>';
+            $xml .= '<url><loc>' . $baseUrl . '/hyderabad/' . $item->slug . '</loc></url>';
+        } 
+        foreach ($categories as $cat) {
+            $xml .= '<url><loc>' . $baseUrl . '/category/' . $cat->slug . '/tirupati</loc></url>';
         }
         foreach ($categories as $cat) {
-            $xml .= '<url><loc>' . url('/category/' . $cat->slug . '/tirupati') . '</loc></url>';
-        }
-        foreach ($categories as $cat) {
-            $xml .= '<url><loc>' . url('/category/' . $cat->slug . '/hyderabad') . '</loc></url>';
+            $xml .= '<url><loc>' . $baseUrl . '/category/' . $cat->slug . '/hyderabad</loc></url>';
         }
         foreach ($stores as $store) {
-            $xml .= '<url><loc>' . url('/tirupati/store/' . $store->slug) . '</loc></url>';
+            $xml .= '<url><loc>' . $baseUrl . '/tirupati/store/' . $store->slug . '</loc></url>';
         }
         foreach ($stores as $store) {
-            $xml .= '<url><loc>' . url('/hyderabad/store/' . $store->slug) . '</loc></url>';
+            $xml .= '<url><loc>' . $baseUrl . '/hyderabad/store/' . $store->slug . '</loc></url>';
         }
          // Add static URLs
-         $xml .= '<url><loc>' . url('/dashboard') . '</loc></url>';
-         $xml .= '<url><loc>' . url('/terms-and-conditions') . '</loc></url>';
-         $xml .= '<url><loc>' . url('/privacy-policy') . '</loc></url>';
-         $xml .= '<url><loc>' . url('/cancellation-policy') . '</loc></url>';
-         $xml .= '<url><loc>' . url('/refund-policy') . '</loc></url>';
-         $xml .= '<url><loc>' . url('/shipping-policy') . '</loc></url>';
+         $xml .= '<url><loc>' . $baseUrl . '/dashboard</loc></url>';
+         $xml .= '<url><loc>' . $baseUrl . '/terms-and-conditions</loc></url>';
+         $xml .= '<url><loc>' . $baseUrl . '/privacy-policy</loc></url>';
+         $xml .= '<url><loc>' . $baseUrl . '/cancellation-policy</loc></url>';
+         $xml .= '<url><loc>' . $baseUrl . '/refund-policy</loc></url>';
+         $xml .= '<url><loc>' . $baseUrl . '/shipping-policy</loc></url>';
 
         $xml .= '</urlset>'; 
 
