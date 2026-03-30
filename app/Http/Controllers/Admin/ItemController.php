2882313b585125e71aa3a3f706bbc837552ee881
afryn <76405702+afryn@users.noreply.github.com>
2026-03-30 16:34:37 +0530

    updated

--- app/Http/Controllers/Admin/ItemController.php
+++ app/Http/Controllers/Admin/ItemController.php
@@ -1327,6 +1327,8 @@ class ItemController extends Controller
         return response()->json(['errors' => Helpers::error_processor($validator)]);
     }
 
+    prx($request->all());
+
     $itemIds = $request->item_id ?? [];
 
     foreach ($request->category as $categoryId) {
