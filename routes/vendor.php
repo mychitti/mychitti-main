9dfde7021db347385da77104cba84f74502024a2
afryn <76405702+afryn@users.noreply.github.com>
2026-05-21 17:05:33 +0530

    Seperated HMIS, POS, Laundry modules

--- routes/vendor.php
+++ routes/vendor.php
@@ -1020,7 +1020,7 @@ Route::group(['namespace' => 'Vendor', 'as' => 'vendor.'], function () {
     // hmis ==============================
     if (file_exists(app_path('Modules/HMIS/routes/vendor.php'))) {
         require app_path('Modules/HMIS/routes/vendor.php');
-    }
+    } 
  
     // laundry ============================== 
     require app_path('Modules/Laundry/routes/vendor.php');
